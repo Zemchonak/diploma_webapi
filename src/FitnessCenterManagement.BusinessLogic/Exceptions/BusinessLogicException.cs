64254da0ac171eb47da54e7b2bb7c8b54ef13261
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-21 00:50:10 +0300

    Add BusinessLogic & Validation exceptions

---
+++ src/FitnessCenterManagement.BusinessLogic/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace FitnessCenterManagement.BusinessLogic.Exceptions
+{
+    [Serializable]
+    public class BusinessLogicException : Exception
+    {
+        public BusinessLogicException(string message)
+            : base(message)
+        {
+        }
+
+        public BusinessLogicException(string message, string fieldName = "", IReadOnlyDictionary<string, int> info = null)
+            : base(message)
+        {
+            FieldName = fieldName;
+            Info = info;
+        }
+
+        public BusinessLogicException(string message, Exception innerException, string fieldName = "", IReadOnlyDictionary<string, int> info = null)
+        : base(message, innerException)
+        {
+            FieldName = fieldName;
+            Info = info;
+        }
+
+        public BusinessLogicException(string message, Exception innerException)
+        : base(message, innerException)
+        {
+        }
+
+        public BusinessLogicException()
+        {
+        }
+
+        protected BusinessLogicException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+
+        public string FieldName { get; set; }
+
+        public IReadOnlyDictionary<string, int> Info { get; set; }
+    }
+}
---
+++ src/FitnessCenterManagement.BusinessLogic/Exceptions/ValidationException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FitnessCenterManagement.BusinessLogic.Exceptions
+{
+    [Serializable]
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message, string fieldName = "")
+            : base(message)
+        {
+            FieldName = fieldName;
+        }
+
+        public ValidationException(string message)
+            : base(message)
+        {
+            FieldName = "";
+        }
+
+        public ValidationException(string message, Exception innerException, string fieldName = "")
+            : base(message, innerException)
+        {
+            FieldName = fieldName;
+        }
+
+        public ValidationException(string message, Exception innerException)
+        : base(message, innerException)
+        {
+        }
+
+        public ValidationException()
+        {
+        }
+
+        protected ValidationException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+
+        public string FieldName { get; set; }
+    }
+}
