c8fd608f5fa0cb8c3525a8c6a5f1153cc7767272
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:05:13 +0300

    Create IdentityConstants.cs

---
+++ src/FitnessCenterManagement.Api/Identity/IdentityConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace FitnessCenterManagement.Api.Identity
+{
+    public static class IdentityConstants
+    {
+        internal const string UserIdClaimType = "UserId";
+
+        internal const string TrainerRole = "Trainer";
+        internal const string UserRole = "User";
+        internal const string ManagerRole = "Manager";
+        internal const string DirectorRole = "Director";
+        internal const string MarketerRole = "Marketer";
+
+        internal const int DefaultLanguageId = 1;
+
+        internal const decimal MinBalance = 0.0m;
+    }
+}
