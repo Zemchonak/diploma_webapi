8156c4ba5dd89dbc406bd20aeeabfc9ce9a502ad
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-22 22:57:06 +0300

    Add generic entity service & its interface

---
+++ src/FitnessCenterManagement.BusinessLogic/Interfaces/IEntityService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FitnessCenterManagement.BusinessLogic.Exceptions;
+using FitnessCenterManagement.DataAccess.Interfaces;
+
+namespace FitnessCenterManagement.BusinessLogic.Interfaces
+{
+    /// <summary>
+    /// An interface that defines basic CRUD operations methods for entity <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The class that implements <see cref="IBasicEntity"/>.
+    /// </typeparam>
+    public interface IEntityService<T>
+        where T : class, IBasicEntity
+    {
+        /// <summary>
+        /// Creates an entity in the database.
+        /// </summary>
+        /// <param name="item">An entity to create.</param>
+        /// <exception cref="ArgumentNullException">exception.</exception>
+        /// <returns>The ID of the created entity.</returns>
+        Task<int> CreateAsync(T item);
+
+        /// <summary>
+        /// Gets the item from the database with the specific ID.
+        /// </summary>
+        /// <param name="id">The specific ID to look for.</param>
+        /// <returns>The found entity.</returns>
+        Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Gets all the entities from the database.
+        /// </summary>
+        /// <returns>The set of the entities.</returns>
+        IQueryable<T> GetAll();
+
+        /// <summary>
+        /// Ensures the entity exists and updates it in the database.
+        /// </summary>
+        /// <param name="item">The entity to update.</param>
+        /// <exception cref="BusinessLogicException">exception.</exception>
+        Task UpdateAsync(T item);
+
+        /// <summary>
+        /// Ensures the entity exists and deletes it from the database.
+        /// </summary>
+        /// <param name="id">The entity to delete.</param>
+        /// <exception cref="BusinessLogicException">exception.</exception>
+        Task DeleteAsync(int id);
+    }
+}
---
+++ src/FitnessCenterManagement.BusinessLogic/Services/GenericEntityService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FitnessCenterManagement.BusinessLogic.Exceptions;
+using FitnessCenterManagement.BusinessLogic.Interfaces;
+using FitnessCenterManagement.DataAccess.Interfaces;
+
+namespace FitnessCenterManagement.BusinessLogic.Services
+{
+    internal class GenericEntityService<T> : IEntityService<T>
+        where T : class, IBasicEntity
+    {
+        private readonly IRepository<T> _repository;
+
+        public GenericEntityService(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> CreateAsync(T item)
+        {
+            try
+            {
+                return await _repository.CreateAsync(item);
+            }
+            catch (ArgumentNullException)
+            {
+                throw new BusinessLogicException(Resources.StringRes.OperateOnNullEntityMsg);
+            }
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            if (entity is null)
+            {
+                throw new BusinessLogicException(Resources.StringRes.NotFoundMsg);
+            }
+
+            return entity;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public async Task UpdateAsync(T item)
+        {
+            try
+            {
+                await _repository.UpdateAsync(item);
+            }
+            catch (ArgumentNullException)
+            {
+                throw new BusinessLogicException(Resources.StringRes.NotFoundMsg);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            try
+            {
+                await _repository.DeleteAsync(id);
+            }
+            catch (ArgumentException)
+            {
+                throw new BusinessLogicException(BusinessLogic.Resources.StringRes.NotFoundMsg);
+            }
+        }
+    }
+}
