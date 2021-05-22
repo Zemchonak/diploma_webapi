57da4e1121b6f543a4904a6590d76e946cecebd7
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-22 23:38:18 +0300

    Create BLL Constants

---
+++ src/FitnessCenterManagement.BusinessLogic/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace FitnessCenterManagement.BusinessLogic
+{
+    internal static class Constants
+    {
+        internal const decimal PriceMinimum = 0m;
+
+        internal const int AbonementAttendancesMinimum = 1;
+        internal const int VisitsMinimum = 1;
+
+        internal const int MeetingsMinutesLengthMinimum = 1;
+        internal const int MeetingsMinutesLengthMaximum = 240;
+
+        internal const decimal CoefficientMinimum = 0;
+        internal const decimal CoefficientMaximum = 2;
+
+        internal const int MinDayOfWeek = 1;
+        internal const int MaxDayOfWeek = 7;
+
+        internal const int MinVisitorCapacity = 0;
+    }
+}
