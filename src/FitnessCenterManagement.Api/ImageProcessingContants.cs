7cd0642973313bb3b7e2e34d57a6d2a308a50a40
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:06:55 +0300

    Create ImageProcessingContants.cs

---
+++ src/FitnessCenterManagement.Api/ImageProcessingContants.cs
@@ -0,0 +1,15 @@
+﻿namespace FitnessCenterManagement.Api
+{
+    public static class ImageProcessingContants
+    {
+        internal const string DefaultAvatarsImagesFolder = "\\Content\\Avatars\\";
+        internal const string DefaultVenuesImagesFolder = "\\Content\\Venues\\";
+        internal const string DefaultAbonementsImagesFolder = "\\Content\\Abonements\\";
+        internal const string DefaultAvatarImageFileName = "defaultAvatar.jpg";
+        internal const string DefaultAvatarImageNotAvailableFileName = "notAvailableAvatar.jpg";
+        internal const string DefaultVenueImageFileName = "defaultVenueImage.jpg";
+        internal const string DefaultVenueImageNotAvailableFileName = "defaultVenueImage.jpg";
+        internal const string DefaultAbonementImageFileName = "defaultAbonementImage.jpg";
+        internal const string DefaultAbonementImageNotAvailableFileName = "defaultAbonementImage.jpg";
+    }
+}
