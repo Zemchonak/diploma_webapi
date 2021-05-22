c2ef2f79bf325131b7912c0701fe99b18a918b0b
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:04:38 +0300

    Create AuthApiDbContext.cs

---
+++ src/FitnessCenterManagement.Api/Contexts/AuthApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FitnessCenterManagement.Api.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessCenterManagement.Api.Contexts
+{
+    public class AuthApiDbContext : IdentityDbContext<User>
+    {
+        public AuthApiDbContext(DbContextOptions<AuthApiDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
