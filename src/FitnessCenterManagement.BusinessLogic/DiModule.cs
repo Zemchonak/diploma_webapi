cdae6c54d48f70696a293ca97dbbc51752c5ad96
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-22 23:38:04 +0300

    Create BLL DI module

---
+++ src/FitnessCenterManagement.BusinessLogic/DiModule.cs
@@ -0,0 +1,25 @@
+﻿using FitnessCenterManagement.BusinessLogic.Interfaces;
+using FitnessCenterManagement.BusinessLogic.Services;
+using FitnessCenterManagement.DataAccess;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitnessCenterManagement.BusinessLogic
+{
+    public static class DiModule
+    {
+        public static void ConfigureBllServices(this IServiceCollection services, string connString)
+        {
+            services.ConfigurateDalServices(connString);
+
+            services.AddScoped(typeof(IEntityService<>), typeof(GenericEntityService<>));
+
+            services.AddScoped<ILocalizationService, LocalizationService>();
+            services.AddScoped<IAbonementsService, AbonementsService>();
+            services.AddScoped<IFitnessCatalogsService, FitnessCatalogsService>();
+            services.AddScoped<ISchedulesService, SchedulesService>();
+            services.AddScoped<IUsersService, UsersService>();
+
+            services.AddScoped<IChatService, ChatService>();
+        }
+    }
+}
