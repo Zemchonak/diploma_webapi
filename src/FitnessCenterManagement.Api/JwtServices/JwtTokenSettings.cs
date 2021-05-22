abca2f4e5adcacd4accd2f265ceb1afd0a292be9
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:12:08 +0300

    Create JWT auth

---
+++ src/FitnessCenterManagement.Api/JwtServices/JwtTokenSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessCenterManagement.Api.JwtServices
+{
+    public class JwtTokenSettings
+    {
+        public string JwtIssuer { get; set; }
+
+        public string JwtAudience { get; set; }
+
+        public string JwtSecretKey { get; set; }
+    }
+}
