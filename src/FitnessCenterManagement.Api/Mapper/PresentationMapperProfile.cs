5939daecbac2c165560da78e549758ba4d605f9f
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:19:16 +0300

    Create PresentationMapperProfile.cs

---
+++ src/FitnessCenterManagement.Api/Mapper/PresentationMapperProfile.cs
@@ -0,0 +1,6 @@
+            // CreateMap<SpecializationModel, SpecializationDto>().ReverseMap()
+            // CreateMap<ServiceModel, ServiceDto>().ReverseMap()
+            // CreateMap<TrainerModel, TrainerDto>().ReverseMap()
+            // CreateMap<TrainerEditModel, TrainerDto>().ReverseMap()
+            // CreateMap<VenueModel, VenueDto>().ReverseMap()
+            // CreateMap<EventModel, FitnessEventDto>().ReverseMap()
