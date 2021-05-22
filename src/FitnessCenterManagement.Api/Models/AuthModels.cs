35e31a2985d4991f7ae36b960d42461ed2560736
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:12:23 +0300

    Create Auth controller & models

---
+++ src/FitnessCenterManagement.Api/Models/AuthModels.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessCenterManagement.Api.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ChangePasswordModel
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
