d9517163ae8d75194c273f361649ad53d9e3ac07
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:19:30 +0300

    Create _OtherModels.cs

---
+++ src/FitnessCenterManagement.Api/Models/_OtherModels.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FitnessCenterManagement.Api.Models
+{
+    public class ChangeLanguageModel
+    {
+        public string LanguageCode { get; set; }
+    }
+
+    public class ErrorViewModel
+    {
+        public string ErrorMessage { get; set; }
+
+        public string ErrorAttribute { get; set; }
+    }
+
+    public class ImageUploadModel
+    {
+        public IFormFile File { get; set; }
+    }
+
+    public class UserModel
+    {
+        public string Id { get; set; }
+
+        public string Surname { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    public class MessageModel
+    {
+        public string Text { get; set; }
+    }
+}
