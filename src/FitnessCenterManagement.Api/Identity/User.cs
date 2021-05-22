a76617dae0b611d2a7fc754a2900f69d79578ff7
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-23 00:05:16 +0300

    Create User.cs

---
+++ src/FitnessCenterManagement.Api/Identity/User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FitnessCenterManagement.Api.Identity
+{
+    public class User : IdentityUser
+    {
+        public string Surname { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public int LanguageId { get; set; }
+
+        public string AvatarName { get; set; }
+
+        public override string NormalizedEmail => Email.ToUpperInvariant();
+
+        public override string NormalizedUserName => UserName.ToUpperInvariant();
+    }
+}
