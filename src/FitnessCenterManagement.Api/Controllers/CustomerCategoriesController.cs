855bbce6304cbe8ac0aac29aa975cb9d17c86615
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-24 15:20:48 +0300

    Create CustomerCategories controller

---
+++ src/FitnessCenterManagement.Api/Controllers/CustomerCategoriesController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FitnessCenterManagement.Api.Identity;
+using FitnessCenterManagement.Api.Models;
+using FitnessCenterManagement.BusinessLogic.Dtos;
+using FitnessCenterManagement.BusinessLogic.Exceptions;
+using FitnessCenterManagement.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenterManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerCategoriesController : ControllerBase
+    {
+        private readonly IUsersService _usersService;
+        private readonly IMapper _mapper;
+
+        public CustomerCategoriesController(IUsersService usersService, IMapper mapper)
+        {
+            _usersService = usersService;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Gets all the customer categories.
+        /// </summary>
+        /// <response code="200">Get is successful, the response contains data about the customer categories.</response>
+        [HttpGet("")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Index(string part = "")
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Unauthorized();
+            }
+
+            var items = await _usersService.GetAllCustomerCategoriesAsync();
+
+            return string.IsNullOrEmpty(part) ?
+                Ok(items) :
+                Ok(items.Where(s => s.Name.Contains(part, System.StringComparison.OrdinalIgnoreCase)).ToList());
+        }
+
+        /// <summary>
+        /// Gets the info about the customer category.
+        /// </summary>
+        /// <response code="200">Get is successful, the response contains data about the customer category.</response>
+        /// <response code="404">The customer category wasn't found.</response>
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> IndexById([FromRoute] int id)
+        {
+            SpecializationModel model;
+
+            try
+            {
+                model = _mapper.Map<SpecializationModel>(await _usersService.GetCustomerCategoryByIdAsync(id));
+            }
+            catch (BusinessLogicException)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        /// <summary>
+        /// Updates the customer category.
+        /// </summary>
+        /// <response code="200">Successful operation.</response>
+        /// <response code="400">There was an error occured while updating the customer category.</response>
+        /// <response code="401">Update of the customer category is available for authorized users.</response>
+        [HttpPut("{id}")]
+        [Authorize(Roles = Identity.IdentityConstants.MarketerRole)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] CustomerCategoryModel model)
+        {
+            try
+            {
+                if (model is null || id != model.Id)
+                {
+                    throw new BusinessLogicException("", fieldName: "", null);
+                }
+
+                await _usersService.UpdateCustomerCategoryAsync(_mapper.Map<CustomerCategoryDto>(model));
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(new ErrorViewModel { ErrorMessage = ex.Message, ErrorAttribute = ex.FieldName });
+            }
+            catch (BusinessLogicException ex)
+            {
+                return BadRequest(new ErrorViewModel { ErrorMessage = ex.Message, ErrorAttribute = ex.FieldName });
+            }
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Creates a customer category.
+        /// </summary>
+        /// <response code="200">Successful operation.</response>
+        /// <response code="400">There was an error occured while creating the customer category.</response>
+        /// <response code="401">Creation of the customer category is available for authorized users.</response>
+        [HttpPost("")]
+        [Authorize(Roles = Identity.IdentityConstants.MarketerRole)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Create([FromBody] CustomerCategoryModel model)
+        {
+            int result;
+
+            try
+            {
+                if (model is null)
+                {
+                    throw new BusinessLogicException("", fieldName: "", null);
+                }
+
+                result = await _usersService.CreateCustomerCategoryAsync(_mapper.Map<CustomerCategoryDto>(model));
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(new ErrorViewModel { ErrorMessage = ex.Message, ErrorAttribute = ex.FieldName });
+            }
+            catch (BusinessLogicException ex)
+            {
+                return BadRequest(new ErrorViewModel { ErrorMessage = ex.Message, ErrorAttribute = ex.FieldName });
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Deletes the customer category.
+        /// </summary>
+        /// <response code="200">Delete is successful.</response>
+        /// <response code="400">There was an error occured while deleting the customer category.</response>
+        /// <response code="401">Deletion of the customer category is available for authorized users.</response>
+        [HttpDelete("{id}")]
+        [Authorize(Roles = Identity.IdentityConstants.MarketerRole)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                await _usersService.DeleteCustomerCategoryAsync(id);
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(new ErrorViewModel { ErrorMessage = ex.Message, ErrorAttribute = ex.FieldName });
+            }
+            catch (BusinessLogicException ex)
+            {
+                return BadRequest(new ErrorViewModel { ErrorMessage = ex.Message, ErrorAttribute = ex.FieldName });
+            }
+
+            return Ok();
+        }
+    }
+}
